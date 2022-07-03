65a8ab6d8a7ae1e1be47d2b46ad86e0d9db2304d
SnowFox-95 <321mts95@gmail.com>
2022-07-03 19:19:21 +0300

    Pep8

--- 3/3_02/3_02_5.py
+++ 3/3_02/3_02_5.py
@@ -1,4 +1,4 @@
-'''
+"""
 Напишите функцию update_dictionary(d, key, value), которая принимает на вход словарь d
 и два числа: key и value.
 
@@ -20,33 +20,34 @@ update_dictionary(d, 2, -2)
 print(d)                            # {2: [-1, -2]}
 update_dictionary(d, 1, -3)
 print(d)                            # {2: [-1, -2, -3]}
-'''
+"""
 
-def update_dictionary(d, key, value):
+
+def update_dictionary(dictionary, key, value):
     # put your python code here
-    if key in d:
-        d[key].append(value)
-        #print('ключ есть')
-    elif key is not d:
-        #d[2*key]=[]
-        if 2*key is d:
-            d[2*key].append(value)
-            #print('ключ 2*key уже есть')
-        elif (2*key is not d) and d.get(2*key)==None:
-            d[2*key]=[]
-            d[2*key].append(value)
-            #print('создание ключа и + новое значение списка')
-        elif (2*key is not d) and d.get(2*key)!=None:
-            d[2*key].append(value)
-            #print('создание ключа и + значение списка')
+    if key in dictionary:
+        dictionary[key].append(value)
+        # print('ключ есть')
+    elif key is not dictionary:
+        # d[2*key]=[]
+        if 2 * key is dictionary:
+            dictionary[2 * key].append(value)
+            # print('ключ 2*key уже есть')
+        elif (2 * key is not dictionary) and dictionary.get(2 * key) is None:
+            dictionary[2 * key] = []
+            dictionary[2 * key].append(value)
+            # print('создание ключа и + новое значение списка')
+        elif (2 * key is not dictionary) and dictionary.get(2 * key) is not None:
+            dictionary[2 * key].append(value)
+            # print('создание ключа и + значение списка')
     return
 
 
 # КОД ДЛЯ ПРОВЕРКИ РАБОТЫ ФУНКЦИИ. НЕ ДЛЯ КОПИИ
 d = {}
 print(update_dictionary(d, 1, -1))  # None
-print(d)                            # {2: [-1]}
+print(d)  # {2: [-1]}
 update_dictionary(d, 2, -2)
-print(d)                            # {2: [-1, -2]}
+print(d)  # {2: [-1, -2]}
 update_dictionary(d, 1, -3)
-print(d)                            # {2: [-1, -2, -3]}
+print(d)  # {2: [-1, -2, -3]}
--- 3/3_02/3_02_6.py
+++ 3/3_02/3_02_6.py
@@ -1,4 +1,4 @@
-'''
+"""
 Когда Антон прочитал «Войну и мир», ему стало интересно, сколько слов и в каком
 количестве используется в этой книге.
 Помогите Антону написать упрощённую версию такой программы, которая сможет подсчитать
@@ -9,27 +9,27 @@
 "слово количество".
 Порядок вывода слов может быть произвольным, каждое уникальное слово должно выводиться
 только один раз.
-'''
+"""
 
 
-def get_words(str):
-    str = str.replace("\n", " ")
-    str = str.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
-    str = str.lower()
-    words = str.split()
-    words.sort()
-    return words
+def get_words(string):
+    string = string.replace("\n", " ")
+    string = string.replace(",", "").replace(".", "").replace("?", "").replace("!", "")
+    string = string.lower()
+    word_from_string = string.split()
+    word_from_string.sort()
+    return word_from_string
 
 
-def get_words_dict(words):
-    words_dict = dict()
+def get_words_dict(words_from_string):
+    words_of_dict = dict()
 
-    for word in words:
-        if word in words_dict:
-            words_dict[word] = words_dict[word] + 1
+    for word_S in words_from_string:
+        if word_S in words_of_dict:
+            words_of_dict[word_S] = words_of_dict[word_S] + 1
         else:
-            words_dict[word] = 1
-    return words_dict
+            words_of_dict[word_S] = 1
+    return words_of_dict
 
 
 str = input()
@@ -37,4 +37,3 @@ words = get_words(str)
 words_dict = get_words_dict(words)
 for word in words_dict:
     print(word.ljust(2), words_dict[word])
-
